a72bf48a97659fddc4324e1bd4e739b6e4a2e0cf
Mohamed <s2000588@edu.bc.fi>
2023-05-06 21:51:37 +0300

    update index.js

--- index.js
+++ index.js
@@ -52,6 +52,16 @@ app.use("/api/v1/categories", categoryRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
